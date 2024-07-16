69a558c21f6fbfeef9b0921bad39c508d65a4b86
Tyler Nie <tylernie@live.ca>
2024-07-16 12:12:50 -0700

    Fixed formatting

--- fdf_to_csv.py
+++ fdf_to_csv.py
@@ -1,6 +1,8 @@
 import re
 import csv
+
 filename = "fdf_to_convert.fdf"
+
 
 def fdf_to_csv(fdf_data, csv_file):
     """
@@ -9,19 +11,21 @@ def fdf_to_csv(fdf_data, csv_file):
     fdf_pattern = re.compile(r"/V\((.*?)\)/T\((.*?)\)", re.DOTALL)
     fields = fdf_pattern.findall(fdf_data)
 
-    with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
+    with open(csv_file, "w", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['Value', 'Field Name'])
+        writer.writerow(["Value", "Field Name"])
         for field in fields:
             writer.writerow(field)
+
 
 def count_rows():
     """
     Counts the number of Fields
     """
-    file = open('output.csv')
+    file = open("output.csv")
     row_count = len(file.readlines())
-    print ("Number of Fields: ", (row_count-1))
+    print("Number of Fields: ", (row_count - 1))
+
 
 def count_checked_boxes():
     """
@@ -29,23 +33,23 @@ def count_checked_boxes():
     word = "Yes"
     count = 0
 
-    with open(filename, 'r', encoding='latin-1') as f:
+    with open(filename, "r", encoding="latin-1") as f:
         fdf_data = f.readlines()
-        for line in fdf_data: 
+        for line in fdf_data:
             l = len(line)
             n = len(word)
             for i in range(l - n):
                 if line[i] == word[0]:
-                    if line[i:i+n] == word:
+                    if line[i : i + n] == word:
                         count += 1
-    print("Number of checked boxes: ", count)      
+    print("Number of checked boxes: ", count)
 
 
 # Read & Write files
-csv_file = 'output.csv'
+csv_file = "output.csv"
 
 # Read FDF file
-with open(filename, 'r', encoding='latin-1') as f:
+with open(filename, "r", encoding="latin-1") as f:
     fdf_data = f.read()
 
 # Convert data to CSV
